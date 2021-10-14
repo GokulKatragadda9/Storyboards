38a625ad1be5553485b0e78c590d5c6e2f9425c2
Gokul Sai Katragadda <gokulsai.katragadda@gmail.com>
2021-10-14 12:55:16 -0400

    Modify CollectionView Flow Layout to modify dell presentation with different trait collections iPhones/iPads

--- Storyboards/ViewControllers/CollectionViewController.swift
+++ Storyboards/ViewControllers/CollectionViewController.swift
@@ -21,6 +21,7 @@ class CollectionViewController: UIViewController {
         super.viewDidLoad()
         collectionView.dataSource = self
         collectionView.delegate = self
+        collectionView.collectionViewLayout = UICollectionViewFlowLayout()
         navigationController?.navigationBar.prefersLargeTitles = true
         title = "Collection View"
     }
@@ -30,6 +31,15 @@ class CollectionViewController: UIViewController {
             destination.number = selectedNumberColorTuple?.0
             destination.color = selectedNumberColorTuple?.1
         }
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        collectionView.collectionViewLayout.invalidateLayout()
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        collectionView.collectionViewLayout.invalidateLayout()
     }
 }
 
@@ -46,12 +56,35 @@ extension CollectionViewController: UICollectionViewDataSource {
     }
 }
 
-extension CollectionViewController: UICollectionViewDelegate {
+extension CollectionViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let selectedNumber = numbersArray[indexPath.item]
         guard let cell = collectionView.cellForItem(at: indexPath) else { return }
         let selectedColor = cell.contentView.backgroundColor ?? .red
         selectedNumberColorTuple = (selectedNumber, selectedColor)
         performSegue(withIdentifier: "DetailViewSegue", sender: self)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var size = collectionView.bounds.width
+        if traitCollection.horizontalSizeClass == .regular && traitCollection.verticalSizeClass == .regular {
+            let columns = 4
+            size -= CGFloat((columns - 1) * 10)
+            let width = size/CGFloat(columns)
+            return CGSize(width: width, height: width)
+        } else {
+            let columns = 7
+            size -= CGFloat((columns - 1) * 10)
+            let width = size/CGFloat(columns)
+            return CGSize(width: width, height: width)
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
     }
 }
